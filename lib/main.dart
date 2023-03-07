562e5fc41cd52bc18782276a8f69992f075a4601
chuongnv <chuongnv@dft.vn>
2023-03-07 17:10:19 +0700

    chat gpt working cmit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'chat_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
